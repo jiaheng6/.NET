533608ac73b9810f1fa7a8a7496a631a0d8752c4
Luocheng <sdsxfjh@163.com>
2018-03-24 11:10:19 +0800

    命名空间相关概念

--- 命名空间的使用/Program.cs
+++ 命名空间的使用/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * .NET FrameWork类库包含大量的类型，用户也可以自定义类型。
+ * 为了有效的组织C#程序中的类型并保证其唯一性，C#引入了命名空间的概念，从而最大限度的避免类型重名错误。
+ * 与文件或组件不同，命名空间是一种逻辑组合。
+ * 在C#文件定义类时，可以把它包括在命名空间的定义中。
+ * C#程序中类型由指示逻辑层次结构的完全限定名（fully qualified name）描述。
+ * 例如：CSharpBook.Chapter01.HelloWorld表示CSharpBook命名空间的子命名空间Chapter01中的HelloWorld类
+ */
+
+
 namespace 命名空间的使用
 {
     class Program
