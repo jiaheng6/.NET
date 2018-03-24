65d04116490012c3d85147e82b21f9e03bf129a7
Luocheng <sdsxfjh@163.com>
2018-03-24 17:56:10 +0800

    值类型引用类型

--- 值类型引用类型和字符串/Program.cs
+++ 值类型引用类型和字符串/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace 值类型引用类型和字符串
 {
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+            
+
+    //        int num = 1000;
+    //        Test(num);
+    //        Console.WriteLine(num); //结果是1000
+    //    }
+    //    public static void Test(int num) 
+    //    {
+    //        num = 100;
+    //        //Console.WriteLine(num);//结果是100
+    //    }
+    //}
     class Program
     {
         static void Main(string[] args)
         {
+
+
+            int num = 1000;
+            Test(ref num);
+            Console.WriteLine(num);//1000
+        }
+        public static void Test(ref int num)
+        {
+            num = 100;
+            Console.WriteLine("test{0}",num);//100
         }
     }
 }
