f1ae9573f56c5e9a9ae991920a21e1062a76783a
Luocheng <sdsxfjh@163.com>
2018-03-24 14:15:04 +0800

    值类型和引用类型

---
+++ 值类型引用类型和字符串/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 值类型引用类型和字符串
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 静态类和静态方法/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 静态类和静态方法
+{
+    class Program
+    {
+        static int num = 10;
+        static void Main(string[] args)
+        {
+            num = 100;
+            Console.WriteLine(num);
+            Show();
+            //Convert就是一个静态类
+        }
+        static void Show() 
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
